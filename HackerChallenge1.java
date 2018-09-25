7c56059273d3a34804773e13aae2ea1aee6b5d6d
ishi17nyu <iv447@nyu.edu>
2018-09-25 00:24:30 -0400

    Added HackerRankChallenge 1

---
+++ HackerChallenge1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class HackerChallenge1 {
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		Double b = scan.nextDouble();
+		scan.nextLine();
+        String s = scan.nextLine();
+
+		
+        System.out.println("String : " + s);
+        System.out.println("Int : " + a);
+        System.out.println("Double : " + b);
+
+    }
+
+
+}
